b866463e46195878495916b9d123d04339adf8a3
Ibrahim Ibrahim <i5ibrahi@ucsd.edu>
2020-01-23 20:36:27 -0800

    First Commit, lets do this

---
+++ tipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalc
+//
+//  Created by Ibrahim A Ibrahim on 1/23/20.
+//  Copyright © 2020 Ibrahim A Ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
